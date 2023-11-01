0811fba46a4985dc920f3a4fac1234c600259024
Vivek Chandra Sardar <chandravivek52@gmail.com>
2023-11-01 11:36:24 +0530

    incorporate pagination and named-query-method findByCategoryId() in the ProductRepository...

--- 02-backend/spring-boot-ecommerce/src/main/java/com/vcs/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/vcs/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.vcs.ecommerce.dao;
 
 import com.vcs.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") long id, Pageable pageable);
 }
